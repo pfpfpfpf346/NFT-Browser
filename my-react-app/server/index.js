6b7fb3fea726ca274325e076b136af8917ea29a7
aaronlim02 <e1123168@u.nus.edu>
2024-06-02 16:50:42 +0800

    backend 4

--- my-react-app/server/index.js
+++ my-react-app/server/index.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
-
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // Enable CORS
 app.use(cors());
@@ -32,6 +31,49 @@ app.get('/api/data', async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
+  }
+});
+
+// register/login
+const jwtSecretKey = process.env.JWT_SECRET;
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const result = await pool.query(
+      'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
+      [username, hashedPassword]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length === 0) {
+      console.log('error length 0');
+      return res.status(400).json({ error: 'Invalid credentials' });
+    }
+
+    const user = result.rows[0];
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      console.log('error no match');
+      return res.status(400).json({ error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ userId: user.id }, jwtSecretKey, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    console.log('error no idk');
+    res.status(400).json({ error: err.message});
   }
 });
 
