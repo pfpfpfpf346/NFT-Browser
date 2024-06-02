859bebaf9936ddfa0757d55729558e2d224754dd
aaronlim02 <e1123168@u.nus.edu>
2024-06-02 20:04:10 +0800

    css stuff

--- my-react-app/src/components/register.js
+++ my-react-app/src/components/register.js
@@ -18,7 +18,7 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleRegister}>
+    <form class="form-center" onSubmit={handleRegister}>
       <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
       <button type="submit">Register</button>
