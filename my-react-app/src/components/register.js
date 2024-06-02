0373f1859e86c8fbd70001bdaabca707eaa4b4eb
aaronlim02 <e1123168@u.nus.edu>
2024-06-02 23:20:04 +0800

    backend

--- my-react-app/src/components/register.js
+++ my-react-app/src/components/register.js
@@ -9,7 +9,11 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('/register', { username, password });
+      const response = await axios.post('/register', { username, password }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'JWT fefege...'
+      }});
       alert('Registration successful');
     } catch (error) {
       console.error('Registration error', error);
