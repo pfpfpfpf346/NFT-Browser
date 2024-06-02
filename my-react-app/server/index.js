05e6bd1cdc5be129eda109adb1102957a634210e
aaronlim02 <e1123168@u.nus.edu>
2024-06-03 00:54:07 +0800

    backend

--- my-react-app/server/index.js
+++ my-react-app/server/index.js
@@ -1,13 +1,69 @@
-const cors = require('cors');
+//server/index.js
 const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
-const port = 5000;
+require('dotenv').config();
 
+const port = process.env.PORT || 5000;
+
+// Enable CORS
+app.use(cors({
+  origin: 'https://nft-browser.vercel.app',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use(express.json());
-app.use(cors());
 
-app.post('/test', (req, res) => {
-  res.send('POST request to /test successful');
+// PostgreSQL pool setup
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// JWT secret key
+const jwtSecretKey = process.env.JWT_SECRET;
+
+// Register endpoint
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const result = await pool.query(
+      'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
+      [username, hashedPassword]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Login endpoint
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: 'Invalid credentials' });
+    }
+
+    const user = result.rows[0];
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ userId: user.id }, jwtSecretKey, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 app.listen(port, () => {
