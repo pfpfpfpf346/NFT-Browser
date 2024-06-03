c2aa3d0d20b3ea06e7a00c8fc53bdee65f71bc8c
aaronlim02 <e1123168@u.nus.edu>
2024-06-03 18:53:00 +0800

    Update register.js

--- my-react-app/api/register.js
+++ my-react-app/api/register.js
@@ -1,17 +1,24 @@
 // api/register.js
-console.log('hi2')
 const { Client } = require('pg');
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 
 // Initialize a PostgreSQL client
-const client = new Client({
-        connectionString: process.env.POSTGRES_URL
-    });
+try {
+    const client = new Client({
+            connectionString: process.env.POSTGRES_URL
+        });
+} catch (e) {
+    console.log('client error')
+}
   
 // Connect to the PostgreSQL database
-client.connect();
-console.log('hi3')
+try {
+    client.connect();
+} catch (e) {
+    console.log('connect error')
+}
+
   
 // Handler function
 module.exports = async (req, res) => {
@@ -20,24 +27,21 @@ module.exports = async (req, res) => {
         return res.status(405).json({ success: false, message: 'Method Not Allowed' });
     }
 
-    console.log('hi4')
-
     const { username, password } = req.body;
-
-    console.log('hi5')
 
     if (!username || !password) {
         return res.status(400).json({ success: false, message: 'Username and password are required' });
     }
 
     try {
+        console.log('hi1')
         const hashedPassword = await bcrypt.hash(password, 10);
         const query = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *';
         const values = [username, hashedPassword];
-    
+        console.log('hi2')
         const dbResponse = await client.query(query, values);
         const newUser = dbResponse.rows[0];
-    
+        console.log('hi3')
         return res.status(201).json({ success: true, user: newUser });
     } catch (error) {
         console.error('Error during registration:', error);
