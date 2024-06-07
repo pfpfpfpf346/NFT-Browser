5f5824dafa68543fc04762b6b04089936c33840e
aaronlim02 <e1123168@u.nus.edu>
2024-06-07 21:19:58 +0800

    wallet explorer

--- nft-nexus-local-ver/client/src/App.js
+++ nft-nexus-local-ver/client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/home';
 import Register from './components/register';
 import PictureButton from './components/PictureButton';
 import AuthButton from './components/AuthButton';
+import WalletExplorer from './components/wallet-explorer';
 import { isAuthenticated } from './utils/auth';
 
 const PrivateRoute = ({ element, ...rest }) => {
@@ -21,6 +22,7 @@ function App() {
           <ul class="nav-links">
             <li><a href="/browse">Browse NFTs</a></li>
             <li><a href="/guides">Guides</a></li>
+            <li><a href="/wallet-explorer">Wallet Explorer</a></li>
             <li><a href="/account">My Profile</a></li>
             <li><AuthButton /></li> {/* Use the new AuthButton component */} {/* change to login button */}
           </ul>
@@ -32,6 +34,7 @@ function App() {
         <Route path="/login" element={<Login />} /> {/* Login page content */}
         <Route path="/account" element={<PrivateRoute element={<Account />} />} /> {/* Account page content */}
         <Route path="/register" element={<Register />} />
+        <Route path="/wallet-explorer" element={<WalletExplorer />} />
         <Route path="*" element={<Navigate to="/" />} /> {/* Redirect unknown routes to home */}
       </Routes>
     </div>
